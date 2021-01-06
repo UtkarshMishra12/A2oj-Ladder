70583920fa704aad204f56023749fd741e5145c7
UtkarshMishra <nasautkarsh@gmail.com>
2021-01-06 16:47:08 +0530

    Ladder Solution-5

---
+++ Ladder 4 - Codeforces Div. 2, A/451A - Game With Sticks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+    ll n,m;cin>>n>>m;
+    ll c=min(n,m);
+    if(c%2==0){
+       cout << "Malvika" << endl;
+	} 
+	else {
+		cout << "Akshat" << endl;
+	}
+    // your code goes here
+	return 0;
+}
